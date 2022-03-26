7fcd75de594ca878721988105ef5343cc2a35d10
Lucimara <lucimara.souza.dev@gmail.com>
2022-03-26 09:30:59 -0300

    Segunda Versão

--- home/models.py
+++ home/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Aluno(models.Model):
+    nome = models.CharField(max_length=30)
+    cpf = models.CharField(max_length=15)
+    status = models.CharField(
+        max_length=1,
+        choices=(
+            ('A', 'Ativo'),
+            ('B', "Bloqueado"),
+            ('C', 'Cancelado'),
+        )
+    )
+
+    def __str__(self):
+        return self.nome
+
+
+class Curso(models.Model):
+    curso = models.CharField(max_length=30)
+    descricao = models.CharField(max_length=100)
+    status = models.CharField(
+        max_length=100,
+        choices=(
+            ('A', 'Ativo'),
+            ('I', "Inativo"),
+        )
+    )
+
+    def __str__(self):
+        return self.curso
